31f4930d0a3195e24beaf3ef6b019a1c0ddde5b8
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-24 16:04:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,6 +2,11 @@ require 'yaml'
 require 'pry'
 
 def load_library(yaml_file)
-  
-  emoticons = {
+  emoticons_hash = {"get_meaning" => {}, "get_name" => {}}
   emo_library = YAML.load_file(yaml_file)
+  emo_library.each do |meaning, value|
+    english = value[0]
+    japanese = value[1]
+    emoticons_hash["get_meaning"][japanese] = meaning
+    emoticons_hash["get_name"][english] = japanese
+end
