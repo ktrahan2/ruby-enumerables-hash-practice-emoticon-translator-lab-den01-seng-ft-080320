c84e80753ad8be16061a7921144eca99003a03b7
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-24 16:44:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,8 +4,8 @@ require 'pry'
 def load_library(yaml_file)
   data = YAML.load_file(yaml_file)
   final_hash = {}
-  data.each_pair do |emoticons, translation|
-    final_hash[emoticons] = {"english" : translation[0], "japanese" : translation[1]}
+  data.each_pair do |meaning, translation|
+    final_hash[meaning] = {"english" : translation[0], "japanese" : translation[1]}
   binding pry
   end
   final_result
