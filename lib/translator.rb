60d0f1d79f43a86063952e6cc0050e0be80d5e40
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-24 17:14:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,7 @@ end
 
 def get_english_meaning(yaml_file, emoticon)
   library = load_library(yaml_file)
-  result = library[final_hash][inner_hash]
+  result = library[final_hash][inner_hash]["english"]
   if result == nil
     return "Sorry, that emoticon was not found"
   end
