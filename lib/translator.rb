6ed9b77842e6db194e002622cf191031170e29e8
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-24 16:36:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,6 +6,7 @@ def load_library(yaml_file)
   final_result = {}
   data.each_pair do |key, translation|
     final_result[key] = {"english" => translation[0], "japanese" => translation[1]}
+  binding pry
   end
   final_result
 end
