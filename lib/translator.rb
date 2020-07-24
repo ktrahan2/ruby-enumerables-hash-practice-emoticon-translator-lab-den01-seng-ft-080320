5693e22bcffa78620affd68af44023e4cef689db
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-24 16:34:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ def load_library(yaml_file)
   data = YAML.load_file(yaml_file)
   final_result = {}
   data.each_pair do |key, translation|
-    final_result[key] = {"english"=> translation[0], "japanese" => translation[1]}
+    final_result[key] = {"english" => translation[0], "japanese" => translation[1]}
   end
   final_result
 end
