41d4bb02ee786f5a42a145993937156b51673883
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-24 16:14:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,13 +4,12 @@ require 'pry'
 def load_library(yaml_file)
   emoticons_hash = {"get_meaning" => {}, "get_name" => {}}
   emo_library = YAML.load_file(yaml_file)
-  binding pry
   emo_library.each do |meaning, value|
     english = value[0]
     japanese = value[1]
     binding pry
-    emoticons_hash["get_meaning"][japanese] = meaning
-    emoticons_hash["get_name"][english] = japanese
+    emoticons_hash["get_meaning"][value[1]] = meaning
+    emoticons_hash["get_name"][value[0] = value[1]
   end
   emoticons_hash
 end
