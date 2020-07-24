4dadb3fc27bbf7a6804605f43a0af3885c49adfe
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-24 16:33:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,10 +1,15 @@
-require 'yaml'
+require "yaml"
 require 'pry'
 
 def load_library(yaml_file)
-  emoticon_hash = {"get_name" => {english => }, }
-  emo_library = YAML.load_file(yaml_file)
+  data = YAML.load_file(yaml_file)
+  final_result = {}
+  data.each_pair do |key, translation|
+    final_result[key] = {"english"=> translation[0], "japanese" => translation[1]}
+  end
+  final_result
+end
+
   
 
   
-end
