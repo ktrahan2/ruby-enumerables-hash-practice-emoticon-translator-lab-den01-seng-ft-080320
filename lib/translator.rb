0d66def7c92565f82a747b1d5aa1bb42619a59ed
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-24 16:04:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,6 +7,7 @@ def load_library(yaml_file)
   emo_library.each do |meaning, value|
     english = value[0]
     japanese = value[1]
+    binding pry
     emoticons_hash["get_meaning"][japanese] = meaning
     emoticons_hash["get_name"][english] = japanese
 end
